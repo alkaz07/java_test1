2e8999a874cdf769bfe62bd48b1e79bc386656ef
Kirill Morozov <kmmorozov@gmail.com>
2022-11-10 19:10:11 +0300

    записан правильный ответ в файле 0 начали рисовать блок-схему

--- Testik.java
+++ Testik.java
@@ -1,3 +1,20 @@
 public class Testik {
-
+    public static void main(String[] args)
+    {
+        //аналитически (не запуская программу) определить, что выводит сей фрагмент кода
+        int a=2, b=14, c = 0;
+        for (c = c + a; c>a - b; c = c - 1)        {
+            System.out.println("a="+a+", b="+b+", c="+c);
+            do  {
+                b = 14;
+                a = a * 4;
+                while (b>a - 2)  {
+                    b = b / 2;
+                    b = b + 1;
+                    System.out.println("a="+a+", b="+b+", c="+c);
+                }
+            } while (a<25);
+        }
+        System.out.println("a="+a+", b="+b+", c="+c);
+    }
 }
